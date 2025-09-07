5079ac26d0d6564818e7859378410d8d6be3f69a
Prawee Wongsa <prawee@hotmail.com>
2025-09-07 22:45:11 +0700

    configure queue on aws

--- lib/q-stack.ts
+++ lib/q-stack.ts
@@ -6,8 +6,16 @@ export class QStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const dlq = new sqs.Queue(this, "QdeadLetterQueue", {
+      retentionPeriod: cdk.Duration.days(7),
+    });
+
     const queue = new sqs.Queue(this, "iQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
+      visibilityTimeout: cdk.Duration.seconds(120),
+      deadLetterQueue: { 
+        queue: dlq, 
+        maxReceiveCount: 3
+      },
     });
 
     new cdk.CfnOutput(this, "QueueUrl", { value: queue.queueUrl });
